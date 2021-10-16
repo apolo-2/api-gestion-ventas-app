1d3b94771330d1409ac9218c10abd51b02b49d6f
hamiltonh <hamiltonhernandez@gmail.com>
2021-10-15 23:16:04 -0500

    instalo dependencias para la autenticacion con oauth0

--- server.js
+++ server.js
@@ -5,17 +5,34 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import Cors from 'cors';
+import morgan from 'morgan';
 import { connectServer } from './db/db.js';
 import rutasProducto from './views/producto/routes.js';
 import rutasUsuario from './views/usuarios/routes.js';
 // import rutasVenta from './views/sale/routes.js';
+import jwt  from 'express-jwt';
+import jwks  from 'jwks-rsa';
 
 dotenv.config({ path: './.env' });
-
 const app = express();
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://misiontic-apolo2-appventas.us.auth0.com/.well-known/jwks.json'
+}),
+audience: 'api-auth-apolo2-ventas-app',
+issuer: 'https://misiontic-apolo2-appventas.us.auth0.com/',
+algorithms: ['RS256']
+});
+
 
 app.use(express.json());
 app.use(Cors());
+app.use(jwtCheck);
+app.use(morgan('tiny'));
 app.use(rutasProducto);
 app.use(rutasUsuario);
 // app.use(rutasVenta);
