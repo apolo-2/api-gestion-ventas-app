c7be0d98a102f4404ce05709fe2f20b0e358dfeb
hamartinezma <hamartinezma@gmail.com>
2021-10-25 11:29:34 -0500

    ajusto conflicto

--- views/producto/routes.js
+++ views/producto/routes.js
@@ -1,56 +1,21 @@
-<<<<<<< HEAD
-import express from 'express';
-=======
 import express from "express";
->>>>>>> ajuste-ventas
 import {
   create,
   remove,
   edit,
   getAll,
-<<<<<<< HEAD
-} from '../../controllers/producto/controller.js';
-=======
   getAvailableProducts,
 } from "../../controllers/producto/controller.js";
->>>>>>> ajuste-ventas
 
 const rutasProducto = express.Router();
 
 const genericCallback = (res, type) => (err, result) => {
-<<<<<<< HEAD
-// const genericCallback = (res) => (err, result) => {
-=======
   // const genericCallback = (res) => (err, result) => {
->>>>>>> ajuste-ventas
 
   if (err) {
     res.status(500).send(err);
   } else {
     // res.json(result);
-<<<<<<< HEAD
-    res.json(type==='PATH'? result.value : result);
-  }
-};
-
-rutasProducto.route('/producto').get((req, res) => {
-  getAll(genericCallback(res));
-});
-
-rutasProducto.route('/producto').post((req, res) => {
-  create(req.body, genericCallback(res));
-});
-
-rutasProducto.route('/producto/:id').patch((req, res) => {
-  edit(req.params.id, req.body, genericCallback(res, 'PATH'));
-});
-
-rutasProducto.route('/producto/:id').delete((req, res) => {
-  remove(req.params.id, genericCallback(res));
-});
-
-export default rutasProducto;
-=======
     res.json(type === "PATH" ? result.value : result);
   }
 };
@@ -78,4 +43,3 @@ rutasProducto.route("/producto/:id").delete((req, res) => {
 });
 
 export default rutasProducto;
->>>>>>> ajuste-ventas
--- views/usuarios/routes.js
+++ views/usuarios/routes.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import Express from 'express';
-=======
 import Express from "express";
->>>>>>> ajuste-ventas
 import {
   todoUsuarios,
   crearUsuario,
@@ -10,72 +6,34 @@ import {
   eliminarUsuario,
   consultarUsuario,
   consultarOCrearUsuario,
-<<<<<<< HEAD
-} from '../../controllers/usuarios/controller.js';
-=======
 } from "../../controllers/usuarios/controller.js";
->>>>>>> ajuste-ventas
 
 const rutasUsuario = Express.Router();
 
 const genercCallback = (res) => (err, result) => {
   if (err) {
-<<<<<<< HEAD
-    res.status(500).send('Error consultando los usuarios');
-=======
     res.status(500).send("Error consultando los usuarios");
->>>>>>> ajuste-ventas
   } else {
     res.json(result);
   }
 };
 
-<<<<<<< HEAD
-rutasUsuario.route('/usuarios').get((req, res) => {
-  console.log('alguien hizo get en la ruta /usuarios');
-=======
 rutasUsuario.route("/usuarios").get((req, res) => {
   console.log("alguien hizo get en la ruta /usuarios");
->>>>>>> ajuste-ventas
   //res.send('alguien hizo get en la ruta /usuarios');
   todoUsuarios(genercCallback(res));
 });
 
-<<<<<<< HEAD
-rutasUsuario.route('/usuarios').post((req, res) => {
-  crearUsuario(req.body, genercCallback(res));
-});
-
-rutasUsuario.route('/usuarios/self').get((req, res) => {
-  console.log('GET en la ruta /self');
-=======
 rutasUsuario.route("/usuarios").post((req, res) => {
   crearUsuario(req.body, genercCallback(res));
 });
 
 rutasUsuario.route("/usuarios/self").get((req, res) => {
   console.log("email");
->>>>>>> ajuste-ventas
   consultarOCrearUsuario(req, genercCallback(res));
   // consultarUsuario(, genercCallback(res));
 });
 
-<<<<<<< HEAD
-rutasUsuario.route('/usuarios/:id').get((req, res) => {
-  console.log('alguien hizo get en la ruta /usuarios');
-  consultarUsuario(req.params.id, genercCallback(res));
-});
-
-rutasUsuario.route('/usuarios/:id').patch((req, res) => {
-  editarUsuario(req.params.id, req.body, genercCallback(res));
-});
-
-rutasUsuario.route('/usuarios/:id').delete((req, res) => {
-  eliminarUsuario(req.params.id, genercCallback(res));
-});
-
-export default rutasUsuario;
-=======
 rutasUsuario.route("/usuarios/:id").get((req, res) => {
   console.log("alguien hizo get en la ruta /usuarios/:id");
   consultarUsuario(req.params.id, genercCallback(res));
@@ -90,4 +48,3 @@ rutasUsuario.route("/usuarios/:id").delete((req, res) => {
 });
 
 export default rutasUsuario;
->>>>>>> ajuste-ventas
